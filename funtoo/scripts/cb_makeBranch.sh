dbbb164f16c877cded8a43390f83d57f49546b73
Shrinidhi Rao <shrinidhi666@gmail.com>
2012-04-08 09:16:24 +0530

    repo name reverted

--- funtoo/scripts/cb_makeBranch.sh
+++ funtoo/scripts/cb_makeBranch.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-rsync -av /home/shrinidhi/bin/gitHub/cb_ports/ /home/shrinidhi/bin/gitHub/cb_ports_locked/ --exclude=/home/shrinidhi/bin/gitHub/cb_ports/.git
+rsync -av /home/shrinidhi/bin/gitHub/cb_ports/ /home/shrinidhi/bin/gitHub/cb_ports_locked/ --exclude=.git
+cd /home/shrinidhi/bin/gitHub/cb_ports_locked/
+git add .
+git commit -a -m "updates for studio branch :)"
+git push -u origin studio
+
+
+
+
  
