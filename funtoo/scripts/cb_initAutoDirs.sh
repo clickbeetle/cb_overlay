b7fd45a0781fef49030600a0ff436d4995ecc56e
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-04-08 14:40:29 +0530

    helper script fixes

--- funtoo/scripts/cb_initAutoDirs.sh
+++ funtoo/scripts/cb_initAutoDirs.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_gentoo/
-mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay/
-mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports/
-mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports_locked/
-mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/portage-gentoo/
-mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/source-trees/
-mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/
+if [ ! -d /BACKUP/clickbeetleCook.DO_NO_DELETE/git/ ]; then 
+	mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/
+fi
 
+if [ ! -d /BACKUP/clickbeetleCook.DO_NO_DELETE/git/source-trees/ ]; then
+	mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/git/source-trees/
+fi
+
+cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/
+
+if [ ! -d /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports/ ]; then
+	git clone git@github.com:clickbeetle/cb_ports.git
+else
+	cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports/
+	git pull
+	cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/
+fi
+
+if [ ! -d /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports_locked/ ]; then
+	git clone git@github.com:clickbeetle/cb_ports_locked.git
+else
+	cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports_locked/
+	git pull
+	cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/
+
+if [ ! -d /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_gentoo/ ]; then
+	git clone git@github.com:clickbeetle/cb_gentoo.git
+else
+	cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_gentoo/
+	git pull 
+	cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/
+fi
 
