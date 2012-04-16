a70cba49e382c9377027ff4bb5c620fdf9c97e49
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-04-16 16:46:34 +0530

    fixing...

--- funtoo/scripts/cb-sync-upstream.py
+++ funtoo/scripts/cb-sync-upstream.py
@@ -27,7 +27,7 @@ for x in fun_pkgs:
   funtoo_pkgs.append(x.lstrip().rstrip())
 for f_pkgs in funtoo_pkgs:
   if(sys.path.exists(cb_overlay.root.rstrip("/") + f_pkgs)):
-    print(f_pkgs + " exists"
+    print(f_pkgs + " exists")
 
 #for ebl in eBuildList:
   #if(ebl):
