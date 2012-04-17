8a1098976b2fabe0be14c44686f5426ff367db9f
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-04-17 12:02:06 +0530

    fixing...

--- funtoo/scripts/cb-sync-upstream.py
+++ funtoo/scripts/cb-sync-upstream.py
@@ -59,7 +59,7 @@ steps = [
   SyncDir(funtoo_overlay.root,"eclass"),
   SyncDir(funtoo_overlay.root,"metadata"),
   SyncDir(funtoo_overlay.root,"profiles","profiles", exclude=["repo_name","categories"]),
-  SyncDir(funtoo_overlay.root,"virtual")
+  SyncDir(funtoo_overlay.root,"virtual"),
   GitPull(branch)
 ]
 
