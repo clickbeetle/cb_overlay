a09ef3c04cc91f85fcfca5e72ab53dfd789c0c99
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-08-01 12:20:40 +0530

    fixing

--- funtoo/scripts/multi-update.sh
+++ funtoo/scripts/multi-update.sh
@@ -9,7 +9,7 @@ die() {
 }
 cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay || die "couldn't chdir"
 git pull
-#./funtoo/scripts/cb-sync-upstream.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay || die "cb_overlay update failed"
+./funtoo/scripts/cb-sync-upstream.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay || die "cb_overlay update failed"
 ./funtoo/scripts/gentoo-update.sh || die "gentoo update failed"
 ./funtoo/scripts/merge.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports || die "cb_ports update failed"
 ./funtoo/scripts/cb_cpl_syncEbuilds.py --branch=studio /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports_locked || die "cb_ports_locked update failed"
