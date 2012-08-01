0bdd9d2ab679b9ec2ef21d0b332603cf09f3f419
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-08-01 12:05:22 +0530

    fixing

--- funtoo/scripts/multi-update-noSync.sh
+++ funtoo/scripts/multi-update-noSync.sh
@@ -10,7 +10,7 @@ die() {
 cd /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay || die "couldn't chdir"
 git pull
 #./funtoo/scripts/gentoo-update.sh || die "gentoo update failed"
-#./funtoo/scripts/cb-sync-upstream.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay || die "cb_overlay update failed"
+./funtoo/scripts/cb-sync-upstream.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay || die "cb_overlay update failed"
 ./funtoo/scripts/merge.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports || die "funtoo update failed cb_ports"
 #./funtoo/scripts/cb_cpl_syncEbuilds.py --branch=studio /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports_locked || die "funtoo update failed cpl"
 rm -f /BACKUP/clickbeetleCook.DO_NO_DELETE/tmp/.regen_running
