dc1f7cfccf477d7a6c8fc977d2308ada57137591
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-09-21 19:01:11 +0530

    fixing

--- funtoo/scripts/multi-update-noSync.sh
+++ funtoo/scripts/multi-update-noSync.sh
@@ -12,5 +12,5 @@ git pull
 #./funtoo/scripts/gentoo-update.sh || die "gentoo update failed"
 ./funtoo/scripts/cb-sync-upstream.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_overlay || die "cb_overlay update failed"
 ./funtoo/scripts/merge.py /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports || die "funtoo update failed cb_ports"
-#./funtoo/scripts/cb_cpl_syncEbuilds.py --branch=studio /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports_locked || die "funtoo update failed cpl"
+./funtoo/scripts/cb_cpl_syncEbuilds.py --branch=studio /BACKUP/clickbeetleCook.DO_NO_DELETE/git/cb_ports_locked || die "funtoo update failed cpl"
 rm -f /BACKUP/clickbeetleCook.DO_NO_DELETE/tmp/.regen_running
