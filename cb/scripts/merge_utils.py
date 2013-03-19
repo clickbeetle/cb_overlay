69f450ec9cc60960688c00150e96a4a2c602dccb
root <shrinidhi666@gmail.com>
2013-03-19 09:20:46 +0530

    fixing argyllcms-1.5.1 libusb to usb

--- cb/scripts/merge_utils.py
+++ cb/scripts/merge_utils.py
@@ -348,7 +348,7 @@ class ProfileDepFix(MergeStep):
 
 class GenCache(MergeStep):
 	def run(self,tree):
-		runShell("egencache --update --portdir=%s --jobs=50" % tree.root, abortOnFail=False)
+		runShell("egencache --update --portdir=%s --jobs=150" % tree.root, abortOnFail=False)
 
 class GitPrep(MergeStep):
 	def __init__(self,branch):
